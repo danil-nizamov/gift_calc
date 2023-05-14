3e8f3dfe5883f2fb6d1b3029ec6c3761101a4b40
danil-nizamov <danil.nizamov+github.codesignal.com>
2023-05-14 04:03:46 +0300

    main functionality

--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import UserStory
 
-# Create your views here.
+
+class UserStoryListView(ListView):
+    model = UserStory
+    template_name = 'main/user_stories.html'
+    context_object_name = 'user_stories'
+    ordering = ['-created_at']
+
+
+class CalcView(TemplateView):
+    template_name = "main/index.html"
+
